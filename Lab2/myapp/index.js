3605ec29ecc52e515dfae426478d63b5256dda91
dawidstyczynski <66594681+dawidstyczynski@users.noreply.github.com>
2022-11-04 14:51:48 +0100

    added insights

--- Lab2/myapp/index.js
+++ Lab2/myapp/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const controller = require('./src/AppControler');
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 const app = express();
 const port = 8080;
 
+const connString = 'InstrumentationKey=583eec4b-b836-4406-8976-bce38ccab47c;IngestionEndpoint=https://northeurope-3.in.applicationinsights.azure.com/;LiveEndpoint=https://northeurope.livediagnostics.monitor.azure.com/'
+const appInsights = new ApplicationInsights({ config: {
+  connectionString: connString
+} });
 
+appInsights.loadAppInsights();
+appInsights.trackPageView(); 
 controller.map(app);
 
 
