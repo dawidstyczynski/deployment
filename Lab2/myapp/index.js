d6eedad8f2047e1259a4303957f3d50ccbb12886
dawidstyczynski <66594681+dawidstyczynski@users.noreply.github.com>
2022-11-04 13:23:43 +0100

    c

--- Lab2/myapp/index.js
+++ Lab2/myapp/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 8080 
+const express = require('express');
+const app = express();
+const port = 8080;
 
 
 app.get("/", (req, res) => {
@@ -9,5 +9,5 @@ app.get("/", (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
