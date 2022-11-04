79a9d013eea305562ec8246efc645e95149343ae
dawidstyczynski <66594681+dawidstyczynski@users.noreply.github.com>
2022-11-04 14:36:54 +0100

    a

--- Lab2/myapp/test/concatTest.js
+++ Lab2/myapp/test/concatTest.js
@@ -4,7 +4,7 @@ var stringModule = require("../src/StringModule");
 describe("Str", function () {
     describe("concat", function () {
       it("should return the concatinated string of two values", function () {
-            var input1 = 'c'
+            var input1 = 'a'
             var input2 = 'b'
             assert.equal(stringModule.concat(input1,input2), input1 + input2);
       });
