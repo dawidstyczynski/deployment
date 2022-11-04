11d60b5a9cf506805bbea12079536684e3d2266d
dawidstyczynski <66594681+dawidstyczynski@users.noreply.github.com>
2022-11-04 13:00:43 +0100

    added concat functionality

--- Lab2/myapp/src/AppControler.js
+++ Lab2/myapp/src/AppControler.js
@@ -1,9 +1,16 @@
 exports.map = function(app){
-    mapHome(app)
+    mapHome(app),
+    mapConcat(app)
 }
 
 function mapHome(app) {
     app.get("/", (req, res) => {
       res.send("Welcome!");
+    });
+  }
+function mapConcat(app) {
+    app.get("/:val1/:val2", (req, res) => {
+  
+      res.send(req.params.val1.toString() +  req.params.val2.toString());
     });
   }
