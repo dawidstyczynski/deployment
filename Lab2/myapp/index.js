0bfcf21856b4ef5f07e809037c52e71363910e04
dawidstyczynski <66594681+dawidstyczynski@users.noreply.github.com>
2022-11-04 13:22:08 +0100

    rb

--- Lab2/myapp/index.js
+++ Lab2/myapp/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const port = 8080 
 
-function mapHome(app) {
-  app.get("/", (req, res) => {
-    res.send("Welcome!");
-  });
-}
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
